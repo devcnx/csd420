fc4fa8b07c1610337ed6e10bc9a65a85fb8b3dc2
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-11-07 15:31:52 -0600

    docs: add Module 3 discussion board snippet templates

---
+++ snippets/module-3/m3_db/Module3DiscussionDemo.java
@@ -0,0 +1,11 @@
+/* file: snippets/module-3/m3_db/Module3DiscussionDemo.java
+ *
+ * Term: Fall 2025  
+ * 
+ * Course: CSD420 Advanced Java Programming
+ * Instructor: Jack Lusby
+ * Institution: Bellevue University
+ *
+ * M3 Discussion Board Snippet
+ * Author: Brittaney Perry-Morgan
+ */
---
+++ snippets/module-3/m3_db/Module3DiscussionDemoIncorrect.java
@@ -0,0 +1,12 @@
+/* file: snippets/module-3/m3_db/Module3DiscussionDemoIncorrect.java
+ *
+ * Term: Fall 2025
+ * 
+ * Course: CSD420 Advanced Java Programming
+ * Instructor: Jack Lusby
+ * Institution: Bellevue University
+ *
+ * M3 Discussion Board Snippet
+ * Author: Brittaney Perry-Morgan
+ */
+
