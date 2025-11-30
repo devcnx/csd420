d92a0a965d7010161670fe3b7e1d81cef7aeeb4a
Brittaney Perry-Morgan <brittaney@devcnx.com>
2025-11-30 10:34:56 -0600

    feat: Add initial JavaFX application and external CSS for Module 7 programming assignment.

---
+++ modules/module-7/Module7ProgrammingAssignment.java
@@ -0,0 +1,30 @@
+/**
+ * file: modules/module-7/Module7ProgrammingAssignment.java
+ *
+ * Term: Fall 2025
+ * 
+ * Course: CSD420 Advanced Java Programming
+ * Instructor: Jack Lusby
+ * Institution: Bellevue University
+ * 
+ * M7 Programming Assignment
+ * Author: Brittaney Perry-Morgan
+ */
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+/**
+ * Demonstration of the integration of JavaFX with an external CSS file.
+ * <p>
+ * This application will display four circles, styled via CSS classes and IDs.
+ * </p>
+ * 
+ * @author Brittaney Perry-Morgan
+ * @version 1.0
+ * @since Fall 2025
+ */
